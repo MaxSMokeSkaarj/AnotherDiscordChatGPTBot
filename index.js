4e78b373ea56a230a7b03df3781cb95ec11dbda6
MaxSMokeSkaarj <vnazarov@smokeofanarchy.ru>
2023-06-18 18:37:48 +1000

    Код рабтает, наконец-то нормально

--- index.js
+++ index.js
@@ -1,127 +1,41 @@
 require( 'dotenv' ).config();
-const { Client, Events, GatewayIntentBits, discordSort } = require( 'discord.js' );
-const { Configuration, OpenAIApi } = require( "openai" );
-const fs = require( 'fs' );
+const { GPT } = require( './lib/driver' );
 
 /** 
  * Токен бота Discord
  * @type {String}
 */
 let DiscordToken = process.env.DiscordToken;
-
 /** 
  * Токен OpenAI
  * @type {String}
 */
 let OpenAIToken = process.env.OpenAIToken;
-
-/** URL OpenAI
+/**
+ * URL OpenAI
  * @type {String}
 */
 let OpenAIURL = process.env.OpenAIURL;
-
-/** Discord channel ID
- * @type {String}
+/**
+ * Discord channel ID
+ * @type {Number}
 */
-let DiscordChannelID = process.env.DiscordChannelID;
-
-/** Модель поведения бота
+let DiscordChannelID = Number( process.env.DiscordChannelID );
+/**
+ * Модель поведения бота
  * @type {String}
 */
 let ChatGPTModel = process.env.ChatGPTModel;
-
-/** ID чата Discord
- * @type {String}
+/**
+ * ID чата Discord
+ * @type {Number}
 */
-let DiscordBotID = process.env.DiscordBotID;
-
+let DiscordBotID = Number( process.env.DiscordBotID );
 /** 
  * Количество Токенов ChatGPT
  * @type {Number}
 */
 let BotTokenAmount = Number( process.env.BotTokenAmount );
 
-// Создаем экземпляр клиента Discord
-/** Экземпляр клиента Discord    */
-const client = new Client( {
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildInvites,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildMessageTyping,
-        GatewayIntentBits.GuildEmojisAndStickers,
-        GatewayIntentBits.GuildIntegrations,
-        GatewayIntentBits.GuildModeration,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.MessageContent,
-    ]
-} );
-
-// Конфигурируем экземпляр OpenAI API
-/** Конфиграция OpenAI */
-const configuration = new Configuration( {
-    apiKey: OpenAIToken,
-    basePath: OpenAIURL,
-} );
-
-// Инициализируем экземпляр OpenAI API
-/** Экземпляр OpenAI */
-const openai = new OpenAIApi( configuration );
-
-// Подключаемся к событию "ready" Discord клиента
-client.on( 'ready', () =>
-{
-    console.log( `Logged in as ${ client.user.tag }` );
-} );
-
-// Подключаемся к событию "message" Discord клиента 
-client.on( 'messageCreate', async ( message ) =>
-{
-
-    /**
-    * История сообщений
-    * @type {Array}
-    */
-    let MessageHistory = [];
-    //let MessageHistory = JSON.parse( fs.readFileSync( './json/history.json' ).toString() );
-    if ( message.channel.id != DiscordChannelID ) return;
-    if ( message.author.id == client.user.id )
-    {
-        MessageHistory.unshift( {
-            role: 'assistant',
-            content: message.content,
-            name: message.author.username.replace( /\s+/g, '_' ).replace( /[^\w\s]/gi, '' ),
-        } );
-        if ( MessageHistory.length > 10 ) MessageHistory.pop();
-        fs.writeFileSync( './json/history.json', JSON.stringify( MessageHistory, '\n', 4 ) );
-    } else
-    {
-        MessageHistory.unshift( {
-            role: 'user',
-            content: message.content,
-            name: message.author.username.replace( /\s+/g, '_' ).replace( /[^\w\s]/gi, '' ),
-        } );
-        if ( MessageHistory.length > 10 ) MessageHistory.pop();
-        fs.writeFileSync( './json/history.json', JSON.stringify( MessageHistory, '\n', 4 ) );
-    }
-
-    if ( message.author.id == client.user.id ) return;
-    await message.channel.sendTyping();
-    // Генерируем ответ с использованием ChatGPT
-    let response = await openai.createChatCompletion( {
-        model: ChatGPTModel,
-        temperature: 0.6,
-        max_tokens: BotTokenAmount,
-        messages: MessageHistory
-    } );
-
-    // Отправляем ответ в Discord канал
-    message.reply( response.data.choices[ 0 ].message.content );
-} );
-
-
-// Запускаем Discord бота
-client.login( DiscordToken );
+let ChatGPT = new GPT( DiscordToken, OpenAIToken, OpenAIURL, DiscordChannelID, ChatGPTModel, DiscordBotID, BotTokenAmount );
+ChatGPT.run();
