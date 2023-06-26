f6d5828b22027817e0b26743a962cb7cdeef8609
MaxSMokeSkaarj <vnazarov@smokeofanarchy.ru>
2023-06-26 23:56:45 +1000

    Переход на ECMAScript

--- index.mjs
+++ index.mjs
@@ -1,36 +1,43 @@
 import { configDotenv } from 'dotenv';
 configDotenv();
 import GPT from './lib/driver.mjs';
+
 /** 
  * Токен бота Discord
  * @type {String}
 */
 let DiscordToken = process.env.DiscordToken;
+
 /** 
  * Токен OpenAI
  * @type {String}
 */
 let OpenAIToken = process.env.OpenAIToken;
+
 /**
  * URL OpenAI
  * @type {String}
 */
 let OpenAIURL = process.env.OpenAIURL;
+
 /**
  * Discord channel ID
- * @type {Number}
+ * @type {String}
 */
-let DiscordChannelID = Number( process.env.DiscordChannelID );
+let DiscordChannelID = process.env.DiscordChannelID;
+
 /**
  * Модель поведения бота
  * @type {String}
 */
 let ChatGPTModel = process.env.ChatGPTModel;
+
 /**
  * ID чата Discord
  * @type {Number}
 */
 let DiscordBotID = Number( process.env.DiscordBotID );
+
 /** 
  * Количество Токенов ChatGPT
  * @type {Number}
@@ -39,5 +46,4 @@ let BotTokenAmount = Number( process.env.BotTokenAmount );
 
 let ChatGPT = new GPT( DiscordToken, OpenAIToken, OpenAIURL, DiscordChannelID, ChatGPTModel, DiscordBotID, BotTokenAmount );
 ChatGPT.run();
-ChatGPT
-
+ChatGPT;
